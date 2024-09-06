709558471a10585a1f9e3055d55ba09b8a2cb601
Kenesary Kalmat <k.kalmat@a1qa.com>
2024-09-06 18:52:07 +0500

    Edit timer test

--- tests/specs/timerTest.spec.js
+++ tests/specs/timerTest.spec.js
@@ -1,6 +1,5 @@
 import { test, expect } from '../../fixtures/page.fixtures.js';
 import * as data from '../../testData/data.json';
-import { Button } from '../../framework/elements/Button.js';
 
 test('Timer test', async ({ mainPage, firstCardPage }) => {
   await expect(mainPage.isPageOpened()).toBeTruthy();
@@ -9,5 +8,5 @@ test('Timer test', async ({ mainPage, firstCardPage }) => {
 
   await expect(firstCardPage.isPageOpened()).toBeTruthy();
   let timerText = await firstCardPage.getFirstCardPageLabelText(data.labelClasses.timerLabel);
-  await expect(timerText).toEqual('00:00:00');
+  await expect(timerText).toEqual(data.timerStart);
 })
