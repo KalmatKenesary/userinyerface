c5b4fcf7975fac619ff4032f4128a95bc2caef07
Kenesary Kalmat <kenekene753@gmail.com>
2026-01-15 14:27:02 +0500

    Merge commits

--- tests/specs/helpFormTest.spec.js
+++ tests/specs/helpFormTest.spec.js
@@ -7,6 +7,9 @@ test('Help form test', async ({ mainPage, firstCardPage }) => {
 
   await mainPage.clickHereButton();
 
+  // second commit
+
+  // third commit
   await expect(firstCardPage.isPageOpened()).toBeTruthy();
 
   await firstCardPage.clickFirstCardPageButton(data.buttonClasses.helpFormButton);
